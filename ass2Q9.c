72929ca03a5d2631bce1c2e9d2b37abb496a6cc7
Mradul Saxena <104089415+Mradul007@users.noreply.github.com>
2023-04-29 20:07:36 +0530

    Add files via upload

---
+++ ass2Q9.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *link;
+};struct node *head,*newnode,*temp;
+void delete_node(struct node *a);
+void main(){
+head = 0;
+int n,i;
+printf("Enter the number of nodes: ");//enter the number of nodes
+scanf("%d",&n);
+//loop to create linked list
+for(i=1;i<=n;i++){
+        newnode = (struct node *)malloc(sizeof(struct node));//creating a single node
+        printf("Enter data in list: ");
+        scanf("%d",&newnode->data);// entering data in the list
+        newnode -> link = NULL;
+if(head == 0){
+        head = temp = newnode;
+}
+else{
+   temp->link = newnode;
+   temp = newnode;
+}
+}
+temp = head;
+printf("Head = %d\n",head);
+for(i=1;i<=n;i++){
+    printf("Data = %d Address = %d Link = %d\n",temp->data,&temp->data,temp->link);
+    temp = temp-> link;
+}
+struct node *a;
+printf("Enter the address of node to delete: ");//the pointer is not  of last node
+scanf("%d",&a);
+delete_node(a);
+temp = head;
+printf("Head = %d\n",head);
+for(i=1;i<=n;i++){
+    printf("Data = %d Address = %d Link = %d\n",temp->data,&temp->data,temp->link);
+    temp = temp-> link;
+}
+}
+void delete_node(struct node *a){
+// If the node to be deleted is the last
+    // node of linked list
+    if (a->link == NULL)
+    {
+        free(a);
+        return;
+    }
+    temp = a->link;
+    a->data = temp->data;
+    a->link = temp->link;
+    free(temp);
+}
